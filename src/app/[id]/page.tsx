7efe2c29a4816470c5fab8c30cd08f8ad2ef3149
Gopal Kedia <gopalkedia@somanirealtors.com>
2024-12-27 11:33:39 +0530

    Fresh Project

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,7 +2,7 @@ import DigitalCard from '@/components/DigitalCard';
 import { employees } from '@/data/employees';
 
 export const metadata = {
-  title: 'Digital Card - Somani Realtors'
+  title: 'Digital Card - Somani Realtors',
 };
 
 type PageParams = {
@@ -13,15 +13,16 @@ interface PageProps {
   params: PageParams;
 }
 
-export default function Page({ params }: PageProps) {
-  const id = params.id;
-  const employeeData = id && employees[id] ? employees[id] : {
+export default async function Page({ params }: PageProps) {
+  const { id } = params;
+
+  const employeeData = employees[id] || {
     firstName: "John",
     lastName: "Doe",
     mobile: "+919830046276",
     email: "contact@somanirealtors.com",
     designation: "Real Estate Consultant",
-    imageUrl: "/api/placeholder/200/200"
+    imageUrl: "/api/placeholder/200/200",
   };
 
   return <DigitalCard employeeData={employeeData} />;
