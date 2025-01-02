533cfcfc85f996fdf20ae92d9e297bc08e1f2b05
Gopal Kedia <gopalkedia@somanirealtors.com>
2025-01-02 13:32:09 +0530

    Fresh Project

--- src/data/employees.ts
+++ src/data/employees.ts
@@ -18,7 +18,7 @@ export const employees: EmployeeDatabase = {
     mobile: "+919830046276",
     email: "gopalkedia@somanirealtors.com",
     designation: "Manager - Operations",
-    imageUrl: "https://somanirealtors.keka.com/files/36781736-e700-4bbd-aeea-afc1b3aae5f8/200x200/profileimage/1b4a0e365020446b86965cd9df864cc3.jpg",
+    imageUrl: "https://somanirealtors.keka.com/files/36781736-e700-4bbd-aeea-afc1b3aae5f8/200x200/profileimage/1b4a0e365020446b86965cd9df864cc3.jpg?1735125417292?1735113600000",
   },
   'CIqAPZ9shdMGgtyU5YewJRoEL7MneF4m': {
     firstName: "Divya",
@@ -26,6 +26,14 @@ export const employees: EmployeeDatabase = {
     mobile: "+919830079859",
     email: "divyaroy@somanirealtors.com",
     designation: "General Manager - HR and Admin",
-    imageUrl: "https://somanirealtors.keka.com/files/36781736-e700-4bbd-aeea-afc1b3aae5f8/200x200/profileimage/f2d06cf8236e4250a1e4b82599449f73.jpeg",
+    imageUrl: "https://somanirealtors.keka.com/files/36781736-e700-4bbd-aeea-afc1b3aae5f8/200x200/profileimage/f2d06cf8236e4250a1e4b82599449f73.jpeg?1735277187231?1735272000000",
   },
+  'uj9bSiHEB94qmp7hNVFYhzqwJuA7QFqt': {
+    firstName: "Amit",
+    lastName: "Roy",
+    mobile: "+919830013815",
+    email: "amitroy@somanirealtors.com",
+    designation: "General Manager - Marketing & Customer Service",
+    imageUrl: "https://somanirealtors.keka.com/files/36781736-e700-4bbd-aeea-afc1b3aae5f8/200x200/profileimage/61fc87a4bb454c46a934a69d48a190c6.jpg?1735804848132?1735804800000",
+  }
 };
